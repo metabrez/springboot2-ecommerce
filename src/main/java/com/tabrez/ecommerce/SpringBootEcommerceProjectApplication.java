4cb671c7b0a6f37716021c2d16bf22f5215a7ac9
Tabrez Alam <metabrez@gmail.com>
2021-01-22 18:24:57 -0500

    basic setup

---
+++ src/main/java/com/tabrez/ecommerce/SpringBootEcommerceProjectApplication.java
@@ -0,0 +1,13 @@
+package com.tabrez.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEcommerceProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEcommerceProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tabrez/ecommerce/SpringBootEcommerceProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tabrez.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEcommerceProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
